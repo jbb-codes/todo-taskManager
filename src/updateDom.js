23058c5a97c01a3d8d3583dbbd532b6fb0a718c5
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-02-28 14:58:38 -0500

    refactor: updateDom.js to clean up code

--- src/updateDom.js
+++ src/updateDom.js
@@ -1,8 +1,27 @@
-export const addProjectToDom = function (todoProject) {
+import { createTodoProject } from "./todo-item";
+
+const addProjectButton = document.querySelector("#add-project-btn");
+addProjectButton.addEventListener("click", () => {
+  const inputProjectName = prompt("Enter the project name:");
+  const todoProjectObj = createTodoProject(inputProjectName);
+  addProjectToDOM(todoProjectObj);
+});
+
+function addProjectToDOM(projectObj) {
   const projectList = document.querySelector(".project-list");
-  const project = document.createElement("li");
-  project.classList.add("project");
-  project.dataset.projectId = todoProject.todoProjectId;
-  project.textContent = todoProject.name;
-  projectList.appendChild(project);
-};
+  const newProject = document.createElement("li");
+  newProject.classList.add("project");
+  newProject.dataset.projectId = projectObj.projectId;
+  newProject.textContent = projectObj.name;
+  projectList.appendChild(newProject);
+  addClickEventToProject(projectObj);
+}
+
+function addClickEventToProject(projectObj) {
+  const project = document.querySelector(
+    "[data-project-id='" + projectObj.projectId + "']"
+  );
+  project.addEventListener("click", () => {});
+}
+
+function createTaskForm() {}
